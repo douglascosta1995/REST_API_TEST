8ca7e33811a23771bcaee1db284b9bc940b8cb3e
Douglas Costa <douglascastro2010@hotmail.com>
2022-08-01 17:23:45 +0200

    firt unit tests

---
+++ tests/unit/models/test_item.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+from models.item import ItemModel
+
+class ItemTest(TestCase):
+    def test_create_item(self):
+        item = ItemModel('test', 19.99)
+
+        self.assertEqual(item.name, 'test',
+                         "The name of the item after creation does not equal the constructor argument")#error message
+        self.assertEqual(item.price, 19.99,
+                         "The price of the item after creation does not equal the constructor argument")
+
+    def test_item_json(self):
+        item = ItemModel('test', 19.99)
+        expected_json = {'name': 'test', 'price': 19.99}
+
+        self.assertEqual(item.json(), expected_json)
